77a0ce99e358dc27d5ab87a07ea58c20095971b7
Larissa Cardoso <larissak.cardosom@gmail.com>
2022-12-31 13:23:54 -0300

    feat: learning conditional

---
+++ javascript/aula010/index.js
@@ -0,0 +1,7 @@
+var vel = 75;
+console.log(`A velocidade do seu carro é de ${vel}Km/h`);
+if(vel > 60){
+    console.log('Você ultrapassou o limite de velocidade. Multado!!')
+} else {
+    console.log('Você está dentro do limite de velocidade.')
+}
