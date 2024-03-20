4e7b7ea4e84d29729ea5623e8d59a7e58045fd02
“larissa-kamily-brisa” <larissa.kamily@grupobrisanet.com.br>
2024-03-20 08:47:08 -0300

    test: improving tests

--- html-e-css/web-componentt/src/components/dropdown/ion-dropdown.spec.js
+++ html-e-css/web-componentt/src/components/dropdown/ion-dropdown.spec.js
@@ -1,5 +1,23 @@
 import { IonDropdown } from "./ion-dropdown.js";
 
+const options = [
+  {
+    label: "Option 1",
+    key: "option1",
+    selected: false,
+  },
+  {
+    label: "Option 2",
+    key: "option2",
+    selected: false,
+  },
+  {
+    label: "Option 3",
+    key: "option3",
+    selected: false,
+  },
+];
+
 describe("IonDropdown", () => {
   let dropdown;
 
@@ -12,52 +30,35 @@ describe("IonDropdown", () => {
   });
 
   it("should have the correct default attributes", () => {
-    expect(dropdown.getAttribute("data-selected")).toBeNull();
-    expect(dropdown.getAttribute("data-options")).toBeNull();
+    expect(dropdown.getAttribute("selected")).toBeNull();
+    expect(dropdown.getAttribute("options")).toBeNull();
   });
 
   it("should update the selected option when an option is clicked", () => {
-    const option1 = {
-      label: "Option 1",
-      key: "option1",
-      selected: false,
-    };
-    const options = [option1];
-    dropdown.setAttribute("options", JSON.stringify(options));
-    expect(dropdown.getAttribute("options")).toBe(JSON.stringify(options));
     const spyHandleClick = jest.spyOn(dropdown, "optionClicked");
 
-    const optionElements = dropdown.querySelector(".item-0");
-    dropdown.optionClicked(option1, 0);
-    expect(option1.selected).toBe(true);
+    dropdown.setAttribute("options", JSON.stringify(options));
+    expect(dropdown.getAttribute("options")).toBe(JSON.stringify(options));
+    dropdown.optionClicked(options[0], 0);
+    expect(options[0].selected).toBe(true);
   });
 
   it("should update the selected option when an option is clicked", () => {
-    const option1 = {
-      label: "Option 1",
-      key: "option1",
-      selected: true,
-    };
-    const options = [option1];
-    dropdown.setAttribute("options", JSON.stringify(options));
-    expect(dropdown.getAttribute("options")).toBe(JSON.stringify(options));
     const spyHandleClick = jest.spyOn(dropdown, "optionClicked");
 
-    const optionElements = dropdown.querySelector(".item-0");
-    dropdown.optionClicked(option1, 0);
-    expect(option1.selected).toBe(false);
+    dropdown.setAttribute("options", JSON.stringify(options));
+    expect(dropdown.getAttribute("options")).toBe(JSON.stringify(options));
+    dropdown.optionClicked(options[0], 0);
+    expect(options[0].selected).toBe(false);
   });
 
   it("should initialize dropdown when connected to the DOM", () => {
-    dropdown._options = ["option1", "option2", "option3"];
-
+    dropdown._options = options;
 
     dropdown.connectedCallback();
 
-
-    expect(dropdown.shadowRoot.getElementById("item-0").onclick).toBeDefined();
-    expect(dropdown.shadowRoot.getElementById("item-1").onclick).toBeDefined();
-    expect(dropdown.shadowRoot.getElementById("item-2").onclick).toBeDefined();
+    expect(dropdown.shadowRoot.getElementById("item-0")).toBeDefined();
+    expect(dropdown.shadowRoot.getElementById("item-1")).toBeDefined();
+    expect(dropdown.shadowRoot.getElementById("item-2")).toBeDefined();
   });
-
 });
